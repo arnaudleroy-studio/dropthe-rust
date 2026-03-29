44457990604a1042f0fd56bd4464d3ce5f219a1d
Arnaud Leroy <dropthehq@gmail.com>
2026-03-29 14:16:23 +0100

    Add .gitignore, remove build artifacts from tracking

--- target/debug/deps/dropthe-1f6dc7ebf46f30e3.d
+++
@@ -1,5 +0,0 @@
-/private/tmp/rust-rewrite/dropthe/target/debug/deps/dropthe-1f6dc7ebf46f30e3.d: src/lib.rs
-
-/private/tmp/rust-rewrite/dropthe/target/debug/deps/dropthe-1f6dc7ebf46f30e3: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/dropthe-72d5a79f57a19db5.d
+++
@@ -1,5 +0,0 @@
-/private/tmp/rust-rewrite/dropthe/target/debug/deps/dropthe-72d5a79f57a19db5.d: src/lib.rs
-
-/private/tmp/rust-rewrite/dropthe/target/debug/deps/libdropthe-72d5a79f57a19db5.rmeta: src/lib.rs
-
-src/lib.rs:
